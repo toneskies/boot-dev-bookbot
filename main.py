49140d13effd7b1ffbb09dee1383f845e6e27266
toneskies <tony.g.badea@gmail.com>
2025-08-26 01:54:40 -0700

    add get_num_letter_count(book)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_letter_count
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,3 +13,5 @@ if __name__ == "__main__":
     # PART 2: Split the string into words and print to console the number of words.
     num_words = get_num_words(text)
     print(f'{num_words} words found in the document.')
+    # PART 3: Get count of letters in book, print out dictionary.
+    get_num_letter_count(text)
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_num_words(book):
     n_words = book.split()
     return len(n_words)
 
+def get_num_letter_count(book):
+    dict_letter_count = {}
+    book_words = book.split()
+    for word in book_words:
+        for letter in word:
+            if letter in dict_letter_count:
+                dict_letter_count[letter] += 1
+            else:
+                dict_letter_count[letter] = 0
+    print(dict_letter_count)
