42dc0d1c7cb83d04799f248580c3c3c8b731040f
toneskies <tony.g.badea@gmail.com>
2025-08-26 02:29:53 -0700

    add report functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, get_num_letter_count
+from stats import get_num_words, get_num_letter_count, get_sorted_dictionaries
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,12 +7,25 @@ def get_book_text(path):
     return file_contents
 
 if __name__ == "__main__":
+    # PART 5: Use sys to take user input for book to read
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    relative_path = sys.argv[1]
     # PART 1: Read the book contents into a string and print it.
-    relative_path = "books/frankenstein.txt"
+    # relative_path = "books/frankenstein.txt"
     text = get_book_text(relative_path)
-    # print(text)
-    # PART 2: Split the string into words and print to console the number of words.
+    # # print(text)
+    # # PART 2: Split the string into words and print to console the number of words.
     num_words = get_num_words(text)
-    print(f'{num_words} words found in the document.')
-    # PART 3: Get count of letters in book, print out dictionary.
-    get_num_letter_count(text)
+    # print(f'{num_words} words found in the document.')
+    # # PART 3: Get count of letters in book, print out dictionary.
+    letter_count_dict = get_num_letter_count(text)
+    # PART 4: Sort Dictionaries and print them out in a list
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f'Found {num_words} total words')
+    print("--------- Character Count -------")
+    get_sorted_dictionaries(letter_count_dict)
+    print("============= END ===============")
--- stats.py
+++ stats.py
@@ -7,8 +7,22 @@ def get_num_letter_count(book):
     book_words = book.split()
     for word in book_words:
         for letter in word:
-            if letter in dict_letter_count:
-                dict_letter_count[letter] += 1
+            if letter.lower() in dict_letter_count:
+                dict_letter_count[letter.lower()] += 1
             else:
-                dict_letter_count[letter] = 0
-    print(dict_letter_count)
+                dict_letter_count[letter.lower()] = 1
+    return dict_letter_count
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_dictionaries(dict):
+    list_of_dicts = []
+    for key, value in dict.items():
+        dict_to_add = {"char": key, "num": value}
+        list_of_dicts.append(dict_to_add)
+        list_of_dicts.sort(reverse=True, key=sort_on)
+    for dict in list_of_dicts:
+        if dict["char"].isalpha():
+            print(f'{dict["char"]}: {dict["num"]}')
+            
